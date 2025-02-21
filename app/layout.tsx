02d91464bd89f47288cae4377f0aaef2f039cebb
devp-with-V <vedant02ms@gmail.com>
2025-02-21 12:01:57 +0530

    e

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Vedant Tendulkar',
+  description: 'Portifolio of Vedant Tendulkar',
+  generator: 'Vedant Tendulkar',
 }
 
 export default function RootLayout({
